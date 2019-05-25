0f00614ee3396570a4c346c70db2668b66d1b938
Mindaugas <mindaugas.bernotas@mediapark.com>
2019-05-25 15:01:27 +0300

    Updates

---
+++ precache-manifest.0b96465e0dedf4dfc6d4bfb99817032d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d564ff01a5b6c3abe835a4c19614ca10",
+    "url": "/react-todoList/index.html"
+  },
+  {
+    "revision": "661b5097e35dd50a8211",
+    "url": "/react-todoList/static/css/main.108c0c39.chunk.css"
+  },
+  {
+    "revision": "c2a91e8d5b145ae0b2bd",
+    "url": "/react-todoList/static/js/2.f1644570.chunk.js"
+  },
+  {
+    "revision": "661b5097e35dd50a8211",
+    "url": "/react-todoList/static/js/main.f1a4bde2.chunk.js"
+  },
+  {
+    "revision": "18f595a1e94b230bde2e",
+    "url": "/react-todoList/static/js/runtime~main.9ab87167.js"
+  }
+]);
