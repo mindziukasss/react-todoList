63b667f33f5a0c895e495ff83310b3571ffaecff
Mindaugas <mindaugas.bernotas@mediapark.com>
2019-05-25 14:08:04 +0300

    Get data from json placeholder

--- src/App.js
+++ src/App.js
@@ -5,29 +5,17 @@ import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
     state = {
-        todos: [
-            {
-                id: uuid.v4(),
-                title: "Take out trasch",
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: "Dinner with wife",
-                completed: true
+        todos: []
+    }
 
-            },
-            {
-                id: uuid.v4(),
-                title: "Meeting with boss",
-                completed: false
-
-            },
-        ]
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({todos: res.data}))
     }
 
     // toggle complete
